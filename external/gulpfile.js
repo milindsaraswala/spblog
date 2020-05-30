d1394521bdaa454187b4056d6d29a616eb2407d4
Milind Saraswala <milindsaraswala@gmail.com>
2020-05-30 18:17:00 +0300

    .

--- external/gulpfile.js
+++ external/gulpfile.js
@@ -1,23 +1,25 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch } = require('gulp')
 
-const targetSpfxFolder = "spfx";
+const targetSpfxFolder = 'spfx'
 
 function copyLocalizedResources() {
-    return src(`../${targetSpfxFolder}/src/webparts/**/loc/*.d.ts`)
-        .pipe(dest('./src/webparts'));
+	return src(`../${targetSpfxFolder}/src/webparts/**/loc/*.d.ts`).pipe(dest('./src/webparts'))
 }
 
 function triggerTargetWebPartReload() {
-    return src(`../${targetSpfxFolder}/src/index.ts`)
-        .pipe(dest(`../${targetSpfxFolder}/src/`))
+	return src(`../${targetSpfxFolder}/src/index.ts`).pipe(dest(`../${targetSpfxFolder}/src/`))
 }
 
-exports['copy-loc'] = copyLocalizedResources;
+exports['copy-loc'] = copyLocalizedResources
 
 exports.watch = function () {
-    watch(`../${targetSpfxFolder}/src/webparts/**/loc/*.d.ts`, {
-        ignoreInitial: false
-    }, copyLocalizedResources);
+	watch(
+		`../${targetSpfxFolder}/src/webparts/**/loc/*.d.ts`,
+		{
+			ignoreInitial: false
+		},
+		copyLocalizedResources
+	)
 
-    watch('./dist/*.js', triggerTargetWebPartReload);
+	watch('./dist/*.js', triggerTargetWebPartReload)
 }
